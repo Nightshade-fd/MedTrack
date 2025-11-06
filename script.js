21a300707bdb9dc5bb9a2894d1face511eb527f1
Muhammed Nazeef <uredoibrahim2@gmail.com>
2025-11-06 22:01:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("JavaScript is connected");
+    
+    const hamburger =
+   document.getElementById("hamburger");
+    const navlinks =
+   document.getElementById("navlinks");
+
+   hamburger.addEventListener("click",()=>{
+    navlinks.classList.toggle("show");
+   });
